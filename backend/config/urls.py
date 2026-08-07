826f31ffcd237fb7856458f1367269d210737d1e
Muhammad-Ahmad-Ishfaq <muhammadahmadishfaq5470@gmail.com>
2026-08-07 11:18:30 +0500

    Fix Django root endpoint and Vercel deployment

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,16 +1,29 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+
+def home(request):
+    return JsonResponse({
+        "message": "ERP Software API is running",
+        "status": "success",
+        "version": "1.0"
+    })
+
 
 urlpatterns = [
+    path('', home),
+
     path('admin/', admin.site.urls),
+
     path('api/users/', include('apps.users.urls')),
-     path('api/', include('apps.inventory.urls')),
+    path('api/', include('apps.inventory.urls')),
     path('api/accounting/', include('apps.accounting.urls')),
-    path('api/core/', include('apps.core.urls')), 
+    path('api/core/', include('apps.core.urls')),
     path('api/vtypes/', include('apps.vtypes.urls')),
-    path('api/accounting/', include('apps.ac_setup.urls')),
+    path('api/ac_setup/', include('apps.ac_setup.urls')),
     path('api/locations/', include('apps.locations.urls')),
-    path("api/purchases/", include("apps.purchases.urls")),
+    path('api/purchases/', include('apps.purchases.urls')),
     path('api/purchase-orders/', include('apps.purchaseOrders.urls')),
     path('api/sales/', include('apps.sales.urls')),
     path('api/sale-orders/', include('apps.saleOrder.urls')),
