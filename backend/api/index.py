d912b78ef76d05264a226e381b256f2111f3b8f0
Muhammad-Ahmad-Ishfaq <muhammadahmadishfaq5470@gmail.com>
2026-08-07 11:09:47 +0500

    Fix Vercel api entrypoint

--- backend/api/index.py
+++ backend/api/index.py
@@ -2,34 +2,15 @@ import os
 import sys
 from pathlib import Path
 
-print("=== VERCEL DJANGO START ===")
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print("BASE DIR:", BASE_DIR)
-
 sys.path.insert(0, str(BASE_DIR))
-
-print("PYTHON PATH:", sys.path)
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "config.settings"
 )
 
-print("Loading Django settings...")
+from django.core.wsgi import get_wsgi_application
 
-try:
-    from django.core.wsgi import get_wsgi_application
-
-    print("Creating Django application...")
-
-    app = get_wsgi_application()
-
-    print("=== DJANGO SUCCESS ===")
-
-except Exception as e:
-    print("=== DJANGO ERROR ===")
-    print(type(e).__name__)
-    print(str(e))
-    raise
+app = get_wsgi_application()
